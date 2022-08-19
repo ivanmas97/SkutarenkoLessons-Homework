3229613b100359b50d4b33d3bc1a302ea37de5b2
ivanmas97 <ivanmas97@yandex.ru>
2022-08-19 18:55:52 +0300

    L10. Homework
    Completed homework part 3, 4, 5*

--- SkutLesson10Functions.playground/Contents.swift
+++ SkutLesson10Functions.playground/Contents.swift
@@ -67,9 +67,17 @@ func whatToDo() -> () -> () {
 let iShouldDoThis = whatToDo()
 iShouldDoThis()
 
+
+
+
+
+
+
+
 //HW part 1: создать пару функций (3-4), которые возвращают строку с каким-то смайликом, вывести в одном принте все функции.
 
-print("\n#1")
+print("\n#1. Unicode symbols.")
+
 func heart() -> (String) {
     return "❤️"
 }
@@ -83,13 +91,16 @@ func books() -> (String) {
 }
 print("\(heart()) \(car()) \(books())")
 
-//HW part 2: функция, которая принимает строку и символ -> либо белая, либо черная (шахматы), распечатать в принте результат переменной
+//HW part 2: функция, которая принимает строку и символ -> либо белая, либо черная (шахматы), распечатать в принте результат переменной.
 
-func chess(lett char: Character, number y: Int) -> String {
+print("\n#2. Chess cell: black or white.")
+
+func chess(letter char: Character, number y: Int) -> String {
     let letters = "abcdefgh"
     var cell = String()
     if let letter = letters.firstIndex(of: char) {
         let x = letter.encodedOffset
+        print(x)
             if (x + y) % 2 == 0 {
                 cell = "White"
             } else {
@@ -99,4 +110,64 @@ func chess(lett char: Character, number y: Int) -> String {
 return cell
 }
 
-    print(chess(lett: "a", number: 1))
+    print(chess(letter: "b", number: 4))
+
+//HW part 3: Cоздать функцию, которая принимает массив/последовательность и возвращает массив в обратном порядке.
+
+print("\n#3. Reversed array.")
+
+func reversedArray(_ initial: [Int]) -> [Int] {
+    var reverse = [Int]()
+    for i in initial.reversed() {
+        reverse.append(i)
+    }
+    return reverse
+}
+print(reversedArray([1, 2, 3, 4, 5]))
+
+// упрощенный вариант
+func reversedArray2(_ initial: Int...) -> [Int] {
+    initial.reversed()
+}
+print(reversedArray2(1, 3, 5, 6, 7))
+
+//HW part 4: Cоздать массив, создать функцию c inout входным массивом, которая не будет ничего возвращать, но будет изменять исходный массив в обратном порядке.
+
+print("\n#4. Reversed inout array.")
+
+func inoutReversed(_ initial: inout [Int]) {
+    initial = initial.reversed()
+}
+
+var array = [2, 4, 6, 8]
+print("Initial array: \(array)")
+
+inoutReversed(&array)
+print("Reversed: \(array)")
+
+
+//HW part 5*: Создать функцию, которая принимает строку и возвращает ее без знаков препинания (_), все гласные переведены в заглавные, все согласные - в строчные, все цифры изменены на слова.
+
+print("\n#5*. String conversion.")
+
+let greeting = "Hello, Mr. James Bond 007!"
+
+func stringConversion(_ initial: String) -> String {
+    var converded = String()
+    let number: [Character: String] = ["0": "Zero", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"]
+    for i in initial {
+        switch i {
+        case "a", "e", "i", "o", "u", "y": converded += i.uppercased()
+        case "Q", "W", "R", "T", "P", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M": converded += i.lowercased()
+        case ",", ".", "!", "?": converded += "_"
+        case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+            if let word = number[i] {
+                converded += word
+            }
+        default: converded.append(i)
+        }
+    }
+    return converded
+}
+
+print(stringConversion(greeting))
